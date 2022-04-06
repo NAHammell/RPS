b29f191b6fd9464baedeef40e3f51607893a69e8
Nick Hammell <nicholas.a.hammell@hotmail.com>
2022-04-06 13:40:31 -0400

    Create  rps.js with computer play function
    Computer play function returns rock, paper, or scissors at random.

---
+++ rps.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+
+    let choice = Math.floor(Math.random() * 3);
+    let play = choices[choice];
+
+    return play;
+}
+
+console.log(computerPlay());
