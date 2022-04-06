4d60118ff219e6a583d7f5d760d8b47734f2d367
Nick Hammell <nicholas.a.hammell@hotmail.com>
2022-04-06 14:09:17 -0400

    Add playRound function with rock

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+// Get a random choice from the computer
+
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"];
 
@@ -7,4 +9,24 @@ function computerPlay() {
     return play;
 }
 
-console.log(computerPlay());
+// Play a round with selection from both player and computer
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You won! Rock beats scissors.";
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+        return "You tied! Rock and rock match.";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lost! Rock loses to paper.";
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
+
+console.log(computerSelection);
