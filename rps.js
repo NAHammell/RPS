3383aa51b1c063de65dd352b4352926da3a980e3
Nick Hammell <nicholas.a.hammell@hotmail.com>
2022-04-06 15:56:23 -0400

    Add scoring system and relevant messages

--- rps.js
+++ rps.js
@@ -15,27 +15,27 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You won! Rock beats scissors.";
+        return ["You won! Rock beats scissors.", 1];
     } else if (playerSelection == "rock" && computerSelection == "rock") {
-        return "You tied! Rock and rock match.";
+        return ["You tied! Rock and rock match.", 0];
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You lost! Rock loses to paper.";
+        return ["You lost! Rock loses to paper.", -1];
     }
 
     if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You won! Paper beats rock.";
+        return ["You won! Paper beats rock.", 1];
     } else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "You tied! Paper and paper match.";
+        return ["You tied! Paper and paper match.", 0];
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lost! Paper loses to scissors.";
+        return ["You lost! Paper loses to scissors.", -1];
     }
 
     if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You won! Scissors beats paper.";
+        return ["You won! Scissors beats paper.", 1];
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "You tied! Scissors and scissors match.";
+        return ["You tied! Scissors and scissors match.", 0];
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You lost! Scissors loses to rock.";
+        return ["You lost! Scissors loses to rock.", -1];
     } else {
         return "An error has occurred...";
     }
@@ -44,11 +44,24 @@ function playRound(playerSelection, computerSelection) {
 // Play a full game, best 3 out of 5
 
 function game() {
+    let score = 0;
+
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt("Rock, paper, or scissors?", "");
         let computerSelection = computerPlay();
 
-        console.log(playRound(playerSelection, computerSelection));
+        let outcome = playRound(playerSelection, computerSelection);
+
+        score += outcome[1];
+        console.log(outcome[0]);
+    }
+
+    if (score > 0) {
+        console.log(`Your score was ${score}, you win!`);
+    } else if (score < 0) {
+        console.log(`Your score was ${score}, you lost...`);
+    } else {
+        console.log(`Your score was ${score}, you tied.`);
     }
 }
 
